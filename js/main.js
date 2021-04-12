bd85c41143c4a1721987362f1ed41ba953787516
Kirill Zhukovskiy <zhuwork@yandex.ru>
2021-04-12 19:50:02 +0300

    Adds some changes

--- js/main.js
+++ js/main.js
@@ -1,46 +1,45 @@
 /*Найди элемент который называется channel-sliper и примени к нему библиотеку Swiper  */
 const swiper = new Swiper('.channel-sliper', {
-  // Optional parameters
-  loop: true /*Сделай так чтобы слайды перелистывались даже когда закончились*/,
-  slidesPerView: 1 /*Сразу видно 6 слайдов*/,
-  spaceBetween: 20,
-  breakpoints: {
-    2100: {
-      slidesPerView: 7,
-    },
-    1920: {
-      slidesPerView: 6,
-    },
+    // Optional parameters
+    loop: true /*Сделай так чтобы слайды перелистывались даже когда закончились*/,
+    slidesPerView: 1 /*Сразу видно 6 слайдов*/,
+    spaceBetween: 20,
+    breakpoints: {
+        2100: {
+            slidesPerView: 7,
+        },
+        1920: {
+            slidesPerView: 6,
+        },
 
-    1600: {
-      slidesPerView: 5,
+        1600: {
+            slidesPerView: 5,
+        },
+        1300: {
+            slidesPerView: 4,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        760: {
+            slidesPerView: 2,
+        },
     },
-    1300: {
-      slidesPerView: 4,
+    // Navigation arrows /*Сделай так чтобы кнопки плагинации работали с классами  swiper-button-next swiper-button-prev' */
+    navigation: {
+        nextEl: '.channel-button-next',
+        prevEl: '.channel-button-prev',
     },
-    1100: {
-      slidesPerView: 3,
-    },
-    760: {
-      slidesPerView: 2,
-    },
-  },
-  // Navigation arrows /*Сделай так чтобы кнопки плагинации работали с классами  swiper-button-next swiper-button-prev' */
-  navigation: {
-    nextEl: '.chennal-button-next',
-    prevEl: '.chennal-button-prev',
-  },
 })
 
-/*
-const swiper = new Swiper('.channel-Recommended',{
+const swiperRecommended = new Swiper('.channel-Recommended',{
  
   loop: true ,
   slidesPerView:4 ,
+  spaceBetween: 50,
 
   navigation: {
-    nextEl: '.chennal-button-next',
-    prevEl: '.chennal-button-prev',
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
   },
 })
-*/
